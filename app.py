a453e0603277597a26793418ccb4e9fba983961a
Anne Schumacher <anne.schumacher.up@gmail.com>
2018-10-16 17:43:19 +0200

    trying to use configparser...

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
+# import configparser
+import yaml
+
 from flask import Flask, send_from_directory
 from flask_restful import Resource, Api
 from flask_cors import CORS
-
-import yaml
+from flask_login import LoginManager
 
 app = Flask(__name__, static_url_path='')
 CORS(app)
 api = Api(app)
+login = LoginManager(app)
 
+# Read config
+app.config.from_pyfile("app_conf.ini")
 
-DATABASE = "database.yaml"
+# import os
+# Config = configparser.ConfigParser()
+# Config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app_conf.ini'))
+# Config.read("app_conf.ini")
 
 
 class Hello(Resource):
@@ -21,7 +29,12 @@ api.add_resource(Hello, '/')
 
 class RecipeList(Resource):
     def get(self):
-        return load_data(DATABASE)
+        # print(app.config)
+        # print(Config.sections())
+        # print(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app_conf.ini'))
+        # return Config.sections()
+        # return load_data(Config["DEFAULT"]["DATABASE"])
+        return load_data("data/database.yaml")
 
 api.add_resource(RecipeList, '/recipe-data')
 
@@ -34,6 +47,29 @@ def send_pdf(path):
 @app.route('/img/<path:path>')
 def send_img(path):
     return send_from_directory('res/img', path)
+
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-v-user-logins
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     # Here we use a class of some kind to represent and validate our
+#     # client-side form data. For example, WTForms is a library that will
+#     # handle this for us, and we use a custom LoginForm to validate.
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         # Login and validate the user.
+#         # user should be an instance of your `User` class
+#         login_user(user)
+#
+#         flask.flash('Logged in successfully.')
+#
+#         next = flask.request.args.get('next')
+#         # is_safe_url should check if the url is safe for redirects.
+#         # See http://flask.pocoo.org/snippets/62/ for an example.
+#         if not is_safe_url(next):
+#             return flask.abort(400)
+#
+#         return flask.redirect(next or flask.url_for('index'))
+#     return flask.render_template('login.html', form=form)
 
 
 def load_data(yamlfile):
