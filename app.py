ddbb54faa894dc014944e12712cbdcf2079d336f
Anne Schumacher <anne.schumacher@gu.se>
2018-10-16 21:49:09 +0200

    config fix (and first attempts to user login)

--- app.py
+++ app.py
@@ -1,40 +1,40 @@
-# import configparser
+# -*- coding: utf-8 -*-
+
+import configparser
 import yaml
 
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
+
+from user import User
 
 app = Flask(__name__, static_url_path='')
 CORS(app)
 api = Api(app)
-login = LoginManager(app)
+# login = LoginManager(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # Read config
-app.config.from_pyfile("app_conf.ini")
-
-# import os
-# Config = configparser.ConfigParser()
-# Config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app_conf.ini'))
-# Config.read("app_conf.ini")
+Config = configparser.ConfigParser()
+Config.read("config.cfg")
 
 
 class Hello(Resource):
     def get(self):
         return {"hello": "true"}
 
+
 api.add_resource(Hello, '/')
 
 
 class RecipeList(Resource):
     def get(self):
-        # print(app.config)
-        # print(Config.sections())
-        # print(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app_conf.ini'))
-        # return Config.sections()
-        # return load_data(Config["DEFAULT"]["DATABASE"])
-        return load_data("data/database.yaml")
+        return load_data(Config.get("DATA", "database"))
+
 
 api.add_resource(RecipeList, '/recipe-data')
 
@@ -48,33 +48,64 @@ def send_pdf(path):
 def send_img(path):
     return send_from_directory('res/img', path)
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    # form = LoginForm()
+    # if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+
+    user = User("anne")
+    login_user(user)
+
+    print('Logged in successfully.')
+
+    # next = request.args.get('next')
+    # is_safe_url should check if the url is safe for redirects.
+    # See http://flask.pocoo.org/snippets/62/ for an example.
+    # if not is_safe_url(next):
+    #     return flask.abort(400)
+
+    return 'Success! User logged in!'
+    # return flask.redirect(next or flask.url_for('index'))
+    # return flask.render_template('login.html', form=form)
+
+
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-v-user-logins
 # @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     # Here we use a class of some kind to represent and validate our
-#     # client-side form data. For example, WTForms is a library that will
-#     # handle this for us, and we use a custom LoginForm to validate.
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         # Login and validate the user.
-#         # user should be an instance of your `User` class
-#         login_user(user)
-#
-#         flask.flash('Logged in successfully.')
-#
-#         next = flask.request.args.get('next')
-#         # is_safe_url should check if the url is safe for redirects.
-#         # See http://flask.pocoo.org/snippets/62/ for an example.
-#         if not is_safe_url(next):
-#             return flask.abort(400)
-#
-#         return flask.redirect(next or flask.url_for('index'))
-#     return flask.render_template('login.html', form=form)
+# def my_login():
+#     if current_user.is_authenticated:
+#         return 'User authenticated!'
+#     username = 'anne'
+#     password = '1234'
+#     remember = True
+#     user = User.query.filter_by(username=username).first()
+#     if user is None or not user.check_password(password):
+#         print('Invalid username or password')
+#         return 'Invalid username or password'
+#     login_user(user, remember=remember)
+#     return 'User %s logged in' % username
+#     # return redirect(url_for('index'))
+#     # return render_template('login.html', title='Sign In', form=form)
 
 
 def load_data(yamlfile):
     with open(yamlfile) as f:
         return yaml.load(f)
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=9005)
+    app.run(
+        debug=Config.getboolean("APP-SETTINGS", "debug_mode"),
+        host=Config.get("APP-SETTINGS", "wsgi_host"),
+        port=Config.get("APP-SETTINGS", "wsgi_port")
+    )
---
+++ user.py
@@ -0,0 +1,37 @@
+
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# from app import login
+
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.username = username
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def is_authenticated(self):
+        # a property that is True if the user has valid credentials or False otherwise.
+        return True
+
+    def is_active(self):
+        # a property that is True if the user's account is active or False otherwise.
+        return True
+
+    def is_anonymous(self):
+        # a property that is False for regular users, and True for a special, anonymous user.
+        return True
+
+    def get_id(self):
+        # a method that returns a unique identifier for the user as a string (unicode, if using Python 2).
+        return True
+
+
+# @login.user_loader
+# def load_user(id):
+#     return User.query.get(int(id))
