c9a7a6e6c873b0814f0a16a5e876fa9b3f8a5cb8
Anne Schumacher <anne.schumacher.up@gmail.com>
2018-10-30 18:14:22 +0100

    login stuff

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import send_from_directory
+from flask import send_from_directory, request, jsonify
 from flask_restful import Resource
 from flask_login import login_user, current_user
 
@@ -35,45 +35,32 @@ def send_img(path):
     return send_from_directory(data_dir, path)
 
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     # Here we use a class of some kind to represent and validate our
-#     # client-side form data. For example, WTForms is a library that will
-#     # handle this for us, and we use a custom LoginForm to validate.
-#     # form = LoginForm()
-#     # if form.validate_on_submit():
-#         # Login and validate the user.
-#         # user should be an instance of your `User` class
-#
-#     user = User("anne", "apa")
-#     a = login_user(user)
-#     print("login user", a)
-#     print("AUTHENTICATED:", user.is_authenticated())
-#     # next = request.args.get('next')
-#     # is_safe_url should check if the url is safe for redirects.
-#     # See http://flask.pocoo.org/snippets/62/ for an example.
-#     # if not is_safe_url(next):
-#     #     return flask.abort(400)
-#
-#     return 'Success! User logged in!'
-#     # return flask.redirect(next or flask.url_for('index'))
-#     # return flask.render_template('login.html', form=form)
-
-
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-v-user-logins
+# https://github.com/maxcountryman/flask-login
 @app.route('/login', methods=['GET', 'POST'])
 def my_login():
+    print("current_user:", current_user)
     if current_user.is_authenticated:
         return 'User authenticated!'
-    username = 'anne'
-    password = 'apa'
-    remember = True
-    # user = User.query.filter_by(username=username).first()
-    user = User(username, password)
-    if not user.is_authenticated():
-        print('Invalid username or password')
-        return 'Invalid username or password'
+    login_data = request.form
+
+    username = login_data["login"]
+    password = login_data["password"]
+    remember = login_data["remember"]
+    user = User()
+    user.id = username
+    user.username = username
+    if not user.is_authenticated(password):
+        return jsonify({"status": "error",
+                        "message": "Invalid username or password!"}), 401
     login_user(user, remember=remember)
-    return 'User %s logged in' % username
+    return jsonify({"status": "success",
+                    "message": "User %s logged in successfully!\n" % username}), 200
     # return redirect(url_for('index'))
     # return render_template('login.html', title='Sign In', form=form)
+
+    # next = request.args.get('next')
+    # is_safe_url should check if the url is safe for redirects.
+    # See http://flask.pocoo.org/snippets/62/ for an example.
+    # if not is_safe_url(next):
+    #     return flask.abort(400)
--- app/user.py
+++ app/user.py
@@ -7,46 +7,43 @@ from db_communicate import UserDB
 
 
 class User(UserMixin):
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-        self.init_user()
+    def __init__(self):
+        self.username = ""
+        self.id = self.username
+        self.user_password = ""
+        self.displayname = ""
 
     def init_user(self):
+        """Retrieve user information from data base."""
         userdb = UserDB()
         userdata = userdb.get(self.username)
-        if not userdata:
-            userdata = (0, "", "", "")
-        self.user_password = userdata[3]
-        self.displayname = userdata[2]
-        self.userid = userdata[0]
+        if userdata:
+            self.user_password = userdata[3]
+            self.displayname = userdata[2]
+            # self.userid = str(userdata[0])
 
-    def is_authenticated(self):
+    def is_authenticated(self, password):
         """Return True if the user has valid credentials (False otherwise)."""
-        return check_password_hash(self.user_password, self.password)
+        self.init_user()
+        return check_password_hash(self.user_password, password)
 
     def is_active(self):
-        # a property that is True if the user's account is active or False otherwise.
+        """Return True if the user's account is active or False otherwise."""
         return True
 
     def is_anonymous(self):
-        # a property that is False for regular users, and True for a special, anonymous user.
-        return True
+        """Return False for regular users, and True for a special, anonymous user."""
+        return False
 
     def get_id(self):
         """Return a unique identifier for the user as a string."""
-        return str(self.userid)
-
-    def get(uid):
-        """Get user by ID."""
-        userdb = UserDB()
-        user = userdb.get_by_id(uid)
-        print("user:", user)
-        # needs to return User object??
-        return user
+        return str(self.username)
 
 
 @login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+def load_user(username):
+    # if username not in users:
+    #     return
+    user = User()
+    user.id = username
+    return user
