746a342260562d83d4143fa8974d8f955bd5c66d
Anne Schumacher <anne.schumacher@gu.se>
2019-02-01 20:16:15 +0100

    change POST data from form to JSON; login fix

--- recapi/views.py
+++ recapi/views.py
@@ -88,10 +88,10 @@ def send_tmp(filename):
 def check_authentication():
     """Check if current user is authorized in the active session."""
     if session.get("authorized"):
-        print("User authorized: %s" % session.get("user"))
-        return utils.success_response("User authorized", user=session.get("user"))
+        current_app.logger.debug("User authorized: %s" % session.get("user"))
+        return utils.success_response("User authorized", authenticated=True, user=session.get("user"))
     else:
-        return utils.success_response("Access denied")
+        return utils.success_response("Access denied", authenticated=False)
 
 
 @general.route('/login', methods=['POST'])
@@ -100,16 +100,18 @@ def login():
     if session.get("authorized"):
         return utils.success_response("User already authorized!")
     else:
-        username = request.form["login"]
-        password = request.form["password"]
+        username = request.get_json().get("login")
+        password = request.get_json().get("password")
+        # username = request.form["login"]
+        # password = request.form["password"]
         user = User(username)
-        current_app.logger.debug("User:", user.username, user.displayname, user.is_authenticated(password))
+        current_app.logger.debug(f"User: {user.username}, {user.displayname}, {user.is_authenticated(password)}")
 
         if user.is_authenticated(password):
             session["authorized"] = True
             session["user"] = user.displayname
             session["uid"] = user.uid
-            print("User %s logged in successfully" % username)
+            current_app.logger.debug("User %s logged in successfully" % username)
             return utils.success_response("User %s logged in successfully!" % username,
                                           user=user.displayname)
         return utils.error_response("Invalid username or password!"), 401
@@ -126,7 +128,7 @@ def logout():
 def preview_data():
     """Generate recipe preview. Convert markdown data to html."""
     try:
-        data = utils.md2htmlform(request.form)
+        data = utils.md2htmlform(request.get_json())
         image_file = request.files.get("image")
         if image_file:
             filename = utils.make_filename(image_file)
