5f9e2b5417192d55f87c6c010a9627f77484f4fa
Anne Schumacher <anne.schumacher@gu.se>
2019-04-29 16:12:22 +0200

    delete removed tags

--- recapi/models/tagmodel.py
+++ recapi/models/tagmodel.py
@@ -29,16 +29,34 @@ class RecipeTags(BaseModel):
 
 
 def add_tags(recipe_data, recipe_id):
-    """Add entries for Tag and RecipeTags."""
+    """Add entries for Tag and RecipeTags and delete removed tags."""
     newTags = recipe_data.get("newTags", {})
     for tagname, category in newTags.items():
+        tagname = tagname.lower().strip()
         tag = Tag(tagname=tagname, parent=TagCategory.get(TagCategory.categoryname == category))
         tag.save()
 
+    # Get existing tags for recipe
+    existing_tags_rows = RecipeTags.select().join(Tag, pw.JOIN.LEFT_OUTER).where((RecipeTags.recipeID == recipe_id))
+    existing_tags = []
+    for row in existing_tags_rows:
+        row_dict = model_to_dict(row)
+        if row_dict.get("tagID", {}).get("tagname"):
+            existing_tags.append(row_dict.get("tagID", {}).get("tagname"))
+
+    # Add tags if they don't exist already
     tags = recipe_data.get("tags", [])
     for tagname in tags:
-        recipetags = RecipeTags(recipeID=Recipe.get(Recipe.id == recipe_id), tagID=Tag.get(Tag.tagname == tagname))
-        recipetags.save()
+        tagname = tagname.lower().strip()
+        if tagname not in existing_tags:
+            recipetags = RecipeTags(recipeID=Recipe.get(Recipe.id == recipe_id), tagID=Tag.get(Tag.tagname == tagname))
+            recipetags.save()
+
+    # Delete removed tags
+    for tagname in existing_tags:
+        if tagname not in tags:
+            recipetags = RecipeTags.get(RecipeTags.recipeID == recipe_id, RecipeTags.tagID == Tag.get(Tag.tagname == tagname).id)
+            recipetags.delete_instance()
 
 
 def get_tags_for_recipe(recipe_id):
