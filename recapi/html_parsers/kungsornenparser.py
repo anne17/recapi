0ccc7047be7a0751bc06576325410681ed618a2e
Anne Schumacher <anne.schumacher@gu.se>
2019-05-02 22:34:40 +0200

    add parser for kungsörnen

---
+++ recapi/html_parsers/kungsornenparser.py
@@ -0,0 +1,77 @@
+"""ICA parser class."""
+
+import re
+import traceback
+
+from flask import current_app
+import html2text
+
+from recapi.html_parsers import GeneralParser
+
+# Set html2text options
+text_maker = html2text.HTML2Text()
+text_maker.emphasis_mark = "*"
+
+
+class KungsornenParser(GeneralParser):
+    """Parser for recipes at kungsornen.se."""
+
+    domain = "kungsornen.se"
+    name = "Kungsörnen"
+    address = "https://www.kungsornen.se/recept/"
+
+    def __init__(self, url):
+        """Init the parser."""
+        self.url = url
+        self.make_soup()
+        self.get_title()
+        self.get_image()
+        self.get_ingredients()
+        self.get_contents()
+        self.get_portions()
+
+    def get_title(self):
+        """Get recipe title."""
+        try:
+            self.title = self.soup.find(class_="theme-headline").text
+        except Exception:
+            current_app.logger.error(f"Could not extract title: {traceback.format_exc()}")
+            self.title = ""
+
+    def get_image(self):
+        """Get recipe main image."""
+        try:
+            image = self.soup.find(class_="container main").find("img").get("src", "")
+            self.image = "https://www.kungsornen.se/" + image
+        except Exception:
+            current_app.logger.error(f"Could not extract image: {traceback.format_exc()}")
+            self.image = ""
+
+    def get_ingredients(self):
+        """Get recipe ingredients list."""
+        try:
+            ingredients = self.soup.find(id="ingredients-list")
+            self.ingredients = text_maker.handle(str(ingredients)).strip("\n")
+        except Exception:
+            current_app.logger.error(f"Could not extract ingredients: {traceback.format_exc()}")
+            self.ingredients = ""
+
+    def get_contents(self):
+        """Get recipe description."""
+        try:
+            contents = self.soup.find(itemprop="recipeInstructions").find_all("p")
+            self.contents = "\n".join(str(i.text) for i in contents)
+            # contents = self.soup.find(itemprop="recipeInstructions")
+            # self.contents = text_maker.handle(str(contents))
+        except Exception:
+            current_app.logger.error(f"Could not extract contents: {traceback.format_exc()}")
+            self.contents = ""
+
+    def get_portions(self):
+        """Get number of portions for recipe."""
+        try:
+            portions = self.soup.find(itemprop="recipeYield").text
+            self.portions = re.sub(r" portion(er)?$", r"", portions)
+        except Exception:
+            current_app.logger.error(f"Could not extract portions: {traceback.format_exc()}")
+            self.portions = ""
