3c4d467d327a2a653e9994dab3a77e6f7ffcfb24
Anne Schumacher <anne.schumacher@gu.se>
2021-01-07 11:17:42 +0100

    update requirements

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from recapi import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, host='localhost', port=9005)
+    app.run(debug=True, host="localhost", port=9005)
